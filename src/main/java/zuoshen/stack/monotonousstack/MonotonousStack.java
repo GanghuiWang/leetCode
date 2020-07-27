682154e1a2ffadd70b5db396e3314ed59df2fa68
ganghuiwang97 <ganghuiwang97@gmail.com>
2020-07-28 00:46:36 +0800

    左神-给定一个不含有重复值的数组 arr，找到每一个 i 位置左边和右边离 i 位置最近且值比 arr[i]小的位置。返回所有位置相应的信息。 --更新main方法中的输出

--- src/main/java/zuoshen/stack/monotonousstack/MonotonousStack.java
+++ src/main/java/zuoshen/stack/monotonousstack/MonotonousStack.java
@@ -3,6 +3,14 @@ package zuoshen.stack.monotonousstack;
 public class MonotonousStack {
     public static void main(String[] args) {
         int[] arr = {3, 4, 1, 5, 6, 2, 7};
+        int[][] result = rightWay(arr);
+        //遍历二维数组
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <result[i].length; j++) {
+                System.out.print(result[i][j]+"  ");
+            }
+            System.out.println("  ");
+        }
 
     }
     public static int[][] rightWay(int[] arr){
