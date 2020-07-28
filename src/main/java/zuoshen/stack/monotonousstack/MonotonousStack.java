0663ae3cb08438c7f142272819e7ee7aea47d296
ganghuiwang97 <ganghuiwang97@gmail.com>
2020-07-29 00:26:15 +0800

    左神-给定一个不含有重复值的数组 arr，找到每一个 i 位置左边和右边离 i 位置最近且值比 arr[i]小的位置。返回所有位置相应的信息。 --更新main方法中的输出 --更新通过栈以O(N)解决问题的方法

--- src/main/java/zuoshen/stack/monotonousstack/MonotonousStack.java
+++ src/main/java/zuoshen/stack/monotonousstack/MonotonousStack.java
@@ -1,13 +1,22 @@
 package zuoshen.stack.monotonousstack;
 
+import java.util.Stack;
+
 public class MonotonousStack {
     public static void main(String[] args) {
         int[] arr = {3, 4, 1, 5, 6, 2, 7};
-        int[][] result = rightWay(arr);
+        int[][] resultByArray = rightWay(arr);
+        int[][] resultByStack = getNearLessNoRepeat(arr);
         //遍历二维数组
         for (int i = 0; i <arr.length ; i++) {
-            for (int j = 0; j <result[i].length; j++) {
-                System.out.print(result[i][j]+"  ");
+                for (int j = 0; j <resultByArray[i].length; j++) {
+                System.out.print(resultByArray[i][j]+"  ");
+            }
+            System.out.println("  ");
+        }
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <resultByStack[i].length; j++) {
+                System.out.print(resultByStack[i][j]+"  ");
             }
             System.out.println("  ");
         }
@@ -40,6 +49,31 @@ public class MonotonousStack {
             }
             res[i][0] = leftLessIndex;
             res[i][1] = rightLessIndex;
+        }
+        return res;
+    }
+    public static int[][]  getNearLessNoRepeat(int[] arr){
+        //定义一个栈
+        Stack<Integer> stack=new Stack<>();
+        //定义结果集
+        int[][] res = new int[arr.length][2];
+        //开始遍历
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty()&&arr[stack.peek()]>arr[i]){
+                int popIndex = stack.pop();
+                //判断stack
+                int leftLessIndex = stack.isEmpty() ? -1 : stack.peek();
+                res[popIndex][0]=leftLessIndex;
+                res[popIndex][1]=i;
+            }
+            stack.push(i);
+        }
+        //当执行完循环，此时i为数组最后一位
+        while (!stack.isEmpty()){
+            int popIndex = stack.pop();
+            int leftIndex = stack.isEmpty() ? -1 : stack.peek();
+            res[popIndex][0] = leftIndex;
+            res[popIndex][1] = -1;
         }
         return res;
     }
