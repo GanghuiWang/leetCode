db3491b85229fcedfa301a8fb79a173aa72b8dc5
ganghuiwang97 <ganghuiwang97@gmail.com>
2020-07-30 00:09:07 +0800

    冒泡排序

--- src/main/java/myself/train/sort/BubbleSort.java
+++ src/main/java/myself/train/sort/BubbleSort.java
@@ -8,19 +8,6 @@ public class BubbleSort {
             System.out.print(result[i] + "  ");
         }
     }
-    //冒泡排序时间数组递增（从左至右由小到大）
-    /*public static int[] bubblSort(int[] arr){
-        for (int i = 0; i <arr.length ; i++) {
-            for (int j = i+1; j <arr.length ; j++) {
-                if(arr[i]>arr[j]){
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
-            }
-        }
-        return arr;
-    }*/
     public static int[] bubblSort(int[] arr){
         //i表示的是趟数
         for (int i = 0; i <arr.length-1 ; i++) {
